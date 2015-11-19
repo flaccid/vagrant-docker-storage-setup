ff14d98b88dbfeb66a5a93b935d39c563f44ec20
Chris Fordham <chris@fordham-nagy.id.au>
2015-11-19 21:42:42 +1100

    Use bash and an array for the overrides, exit if the volume group already exists, use tarball instead of git clone, optional yum update, install docker if needed, remove incorrectly placed docker start.

--- docker-storage-setup-strap.sh
+++ docker-storage-setup-strap.sh
@@ -1,56 +1,75 @@
-#!/bin/sh -ex
+#! /bin/bash -e
+
+# the docker-storage-setup overrides
+overrides=(
+  STORAGE_DRIVER=devicemapper
+  DEVS=/dev/sdb
+  VG=dockervg
+  DATA_SIZE=40%FREE
+  MIN_DATA_SIZE=2G
+  CHUNK_SIZE=512K
+  GROWPART=false
+  AUTO_EXTEND_POOL=yes
+  POOL_AUTOEXTEND_THRESHOLD=60
+  POOL_AUTOEXTEND_PERCENT=20
+)
 
 if vgdisplay | grep dockervg; then
-  echo 'It looks like dockervg volume group already exists, skipping.'
+  echo 'Looks like the dockervg volume group already exists, skipping.'
   exit 0
 fi
 
+# this is recommended but an up-to-date image should be fine
+[[ "$YUM_UPDATE" -eq 1 ]] && yum -y update
+
+# quick install of docker if not found
+if ! type -P docker > /dev/null 2>&1; then
+  cat >/etc/yum.repos.d/docker.repo <<-EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+  yum -y install docker-engine
+fi
+
 # install deps
-yum -y install rpm-build git
+yum -y install rpm-build tar curl
 
-# the docker-storage-setup overrides
-STORAGE_DRIVER=devicemapper
-DEVS=/dev/sdb
-VG=dockervg
-DATA_SIZE=40%FREE
-MIN_DATA_SIZE=2G
-CHUNK_SIZE=512K
-GROWPART=false
-AUTO_EXTEND_POOL=yes
-POOL_AUTOEXTEND_THRESHOLD=60
-POOL_AUTOEXTEND_PERCENT=20
-
-# build and install the rpm
+# download docker-storage-setup
 cd /tmp
-[ -e /tmp/ ] && rm -Rf /tmp/docker-storage-setup
-git clone https://github.com/projectatomic/docker-storage-setup.git
-cd docker-storage-setup
-mkdir -pv /root/rpmbuild/SOURCES
+rm -Rf /tmp/docker-storage-setup*
+curl -L https://github.com/projectatomic/docker-storage-setup/archive/master.tar.gz > \
+  docker-storage-setup.tar.gz
+tar zxvf docker-storage-setup.tar.gz
+cd docker-storage-setup-master
 
 # temp hacks (don't check VG_EXISTS, turn on bash -x)
 sed -i'' '315,318 s/^/#/' docker-storage-setup.sh
 sed -i 's%#!/bin/bash%#!/bin/bash -x%g' docker-storage-setup.sh
 
+# build and install the docker-storage-setup rpm
+mkdir -p /root/rpmbuild/SOURCES
 rm -Rf /root/rpmbuild/SOURCES/*
-cp -v docker-storage-setup.sh /root/rpmbuild/SOURCES/
-cp -v docker-storage-setup.service /root/rpmbuild/SOURCES/
-cp -v docker-storage-setup.conf /root/rpmbuild/SOURCES/
-cp -v docker-storage-setup-override.conf /root/rpmbuild/SOURCES/
-cp -v libdss.sh /root/rpmbuild/SOURCES/
+cp docker-storage-setup.sh \
+  docker-storage-setup.service \
+  docker-storage-setup.conf \
+  docker-storage-setup-override.conf \
+  libdss.sh \
+    /root/rpmbuild/SOURCES/
 rpmbuild -v -bb --clean docker-storage-setup.spec
 rpm -iv --force --replacepkgs /root/rpmbuild/RPMS/x86_64/*.rpm
 
 # save the overrides config
-echo STORAGE_DRIVER=devicemapper >> /etc/sysconfig/docker-storage-setup
-echo DEVS="$DEVS" >> /etc/sysconfig/docker-storage-setup
-echo VG="$VG" >> /etc/sysconfig/docker-storage-setup
-echo DATA_SIZE="$DATA_SIZE" >> /etc/sysconfig/docker-storage-setup
-echo MIN_DATA_SIZE="$MIN_DATA_SIZE" >> /etc/sysconfig/docker-storage-setup
-echo CHUNK_SIZE="$CHUNK_SIZE" >> /etc/sysconfig/docker-storage-setup
-echo GROWPART="$GROWPART" >> /etc/sysconfig/docker-storage-setup
-echo AUTO_EXTEND_POOL="$AUTO_EXTEND_POOL" >> /etc/sysconfig/docker-storage-setup
-echo POOL_AUTOEXTEND_THRESHOLD="$POOL_AUTOEXTEND_THRESHOLD" >> /etc/sysconfig/docker-storage-setup
-echo POOL_AUTOEXTEND_PERCENT="$POOL_AUTOEXTEND_PERCENT" >> /etc/sysconfig/docker-storage-setup
+touch /etc/sysconfig/docker-storage-setup
+for i in "${overrides[@]}"
+do
+    :
+    ! grep "$i" /etc/sysconfig/docker-storage-setup && \
+        echo "$i" >> /etc/sysconfig/docker-storage-setup
+done
 
 # docker should already be enabled, but to be sure
 systemctl enable docker
@@ -60,14 +79,10 @@ systemctl stop docker
 
 # start the docker-storage-setup service to reconfigure the disk
 # systemctl start docker-storage-setup
-# or, command directly:
+#   or, command directly:
 /usr/bin/docker-storage-setup
 
-# finally, start docker
-systemctl start docker
-
 touch /etc/sysconfig/docker /etc/sysconfig/docker-storage /etc/sysconfig/docker-network
-
 mkdir -p /etc/systemd/system/docker.service.d
 cat <<'EOF'> /etc/systemd/system/docker.service.d/10-parameterized.conf
 [Service]
@@ -83,9 +98,7 @@ ExecStart=/usr/bin/docker daemon $OPTIONS \
 EOF
 
 systemctl daemon-reload
-
-# known to not always work first time, or even the second time!
-# e.g. level=fatal msg="zError starting daemon: error initializing graphdriver: Base Device UUID verification failed. Possibly using a different thin pool than last invocation:Er
-systemctl start docker || systemctl restart docker || systemctl restart docker
+# rm -Rf /var/lib/docker/*
+systemctl start docker
 
 echo 'Done.'
