f176f31088a7380d850b4895709956f718dfc2e3
Chris Fordham <chris@fordham-nagy.id.au>
2015-11-19 22:21:54 +1100

    Move the shell script into /data and use a real bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,7 @@
-./docker-storage-setup-strap.sh
+#! /bin/sh -e
+
+bash /tmp/kitchen/data/docker-storage-setup-strap.sh
+
+# if we get this far, lets do some tests
+# https://github.com/test-kitchen/test-kitchen/issues/331
+curl -L https://www.chef.io/chef/install.sh | sudo bash
