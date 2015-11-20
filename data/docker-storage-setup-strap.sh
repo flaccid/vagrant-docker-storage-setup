38feb3acb26258fc159dc4c41bddb23c42074cb8
Chris Fordham <chris@fordham-nagy.id.au>
2015-11-20 17:57:34 +1100

    Re-factor to use an env file with whitelisted directives; print docker info at end.

--- data/docker-storage-setup-strap.sh
+++ data/docker-storage-setup-strap.sh
@@ -1,18 +1,6 @@
 #! /bin/bash -e
 
-# the docker-storage-setup overrides
-overrides=(
-  STORAGE_DRIVER=devicemapper
-  DEVS=/dev/sdb
-  VG=dockervg
-  DATA_SIZE=40%FREE
-  MIN_DATA_SIZE=2G
-  CHUNK_SIZE=512K
-  GROWPART=false
-  AUTO_EXTEND_POOL=yes
-  POOL_AUTOEXTEND_THRESHOLD=60
-  POOL_AUTOEXTEND_PERCENT=20
-)
+[ -e docker-storage-setup-env.sh ] && . docker-storage-setup-env.sh
 
 if vgdisplay | grep dockervg; then
   echo 'Looks like the dockervg volume group already exists, skipping.'
@@ -67,11 +55,24 @@ rpm -iv --force --replacepkgs /root/rpmbuild/RPMS/x86_64/*.rpm
 
 # save the overrides config
 touch /etc/sysconfig/docker-storage-setup
-for i in "${overrides[@]}"
+valid_directives=(
+  STORAGE_DRIVER
+  DEVS
+  VG
+  ROOT_SIZE
+  DATA_SIZE
+  MIN_DATA_SIZE
+  CHUNK_SIZE
+  GROWPART
+  AUTO_EXTEND_POOL
+  POOL_AUTOEXTEND_THRESHOLD
+  POOL_AUTOEXTEND_PERCENT
+)
+for i in "${valid_directives[@]}"
 do
     :
-    ! grep "$i" /etc/sysconfig/docker-storage-setup && \
-        echo "$i" >> /etc/sysconfig/docker-storage-setup
+    [ ! -z "${!i}" ] && ! grep "$i=${!i}" /etc/sysconfig/docker-storage-setup && \
+      echo "$i=${!i}" >> /etc/sysconfig/docker-storage-setup
 done
 
 # docker should already be enabled, but to be sure
@@ -103,5 +104,8 @@ EOF
 systemctl daemon-reload
 # rm -Rf /var/lib/docker/*
 systemctl start docker
+
+# finally, print the docker info
+docker info
 
 echo 'Done.'
