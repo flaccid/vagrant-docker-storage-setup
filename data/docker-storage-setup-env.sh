0613d7b749b6bfffc6094b1c382898d235c884f1
Chris Fordham <chris@fordham-nagy.id.au>
2015-11-20 18:40:12 +1100

    Remove initial override directives that are of same value from the conf in the package.

--- data/docker-storage-setup-env.sh
+++ data/docker-storage-setup-env.sh
@@ -2,15 +2,7 @@
 
 # the docker-storage-setup initial overrides
 
-STORAGE_DRIVER=devicemapper
 DEVS=/dev/sdb
 VG=dockervg
-DATA_SIZE=40%FREE
-MIN_DATA_SIZE=2G
-CHUNK_SIZE=512K
-GROWPART=false
-AUTO_EXTEND_POOL=yes
-POOL_AUTOEXTEND_THRESHOLD=60
-POOL_AUTOEXTEND_PERCENT=20
 
 [ -e docker-storage-setup-env.local.sh ] && . docker-storage-setup-env.local.sh
