09dc06fcca4a37258fc8802ac7512b70bea21678
Chris Fordham <chris@fordham-nagy.id.au>
2015-11-20 17:57:44 +1100

    Add basic tests for docker info.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,3 +10,9 @@ describe service('docker') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe command('docker info') do
+  its(:stdout) { should contain 'Storage Driver: devicemapper' }
+  its(:stdout) { should contain 'Pool Name: dockervg-docker--pool' }
+  its(:stdout) { should_not contain '/dev/loop' }
+end
